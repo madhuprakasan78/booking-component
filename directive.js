5877a2d578a8df2196ecd3208ae2a8f7570e12c1
madhuprakasan78 <madhu.prakasan@gmail.com>
2016-01-18 14:11:14 +0400

    changed the template

--- directive.js
+++ directive.js
@@ -1,6 +1,6 @@
 angular.module('madhu.booking-directive', []).
   directive('w3TestDirective', function () {
      return {
-        template : "<h1>EMPACT components!</h1>"
+        template : "<h1>EMPACT components!</h1><input type='text' style='width:200px'>This is an input box</input>"
     };
   });
