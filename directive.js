ecd6fdaa84fdbdb6e5dc4e4801fa7549be0427cd
madhuprakasan78 <madhu.prakasan@gmail.com>
2016-01-18 15:05:56 +0400

    added padding

--- directive.js
+++ directive.js
@@ -1,6 +1,6 @@
 angular.module('madhu.booking-directive', []).
   directive('w3TestDirective', function () {
      return {
-        template : "<div style='margin-width:10px'><br/><h1>EMPACT components!</h1><input type='text' style='width:200px' value='This is an input box'></input><br/></div>"
+        template : "<div style='padding:10px'><br/><h1>EMPACT components!</h1><input type='text' style='width:200px' value='This is an input box'></input><br/></div>"
     };
   });
