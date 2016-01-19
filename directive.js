675f7af415641283cbfc299363cf2f80ba0b1983
madhuprakasan78 <madhu.prakasan@gmail.com>
2016-01-19 09:56:11 +0400

    Changed the directive name

--- directive.js
+++ directive.js
@@ -1,6 +1,6 @@
 angular.module('madhu.booking-directive', []).
-  directive('w3TestDirective', function () {
+  directive('bookingTestDirective', function () {
      return {
-        template : "<div style='padding:10px'><br/><h1>EMPACT components!</h1><input type='text' style='width:200px' value='This is an input box'></input><br/></div>"
+        template : "<div style='padding:10px'><br/><h1>EMPACT components here!</h1><input type='text' style='width:200px' value='This is an input box'></input><br/></div>"
     };
   });
